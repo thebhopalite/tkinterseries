c94a1d5c86d391298617a34631394efc7315ce9e
Saksham Pathak <82039623+thebhopalite@users.noreply.github.com>
2021-07-11 09:38:01 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root=Tk()
+
+MyLabel=Label(root,text="Hello World").grid(row=0,column=0)
+MyLabel1=Label(root,text="Hello World, I am Sniper").grid(row=1,column=1)
+
+
+
+
+root.mainloop()
